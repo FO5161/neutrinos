eb4de652960d1b6804c93a0beb3e9e2d2d1b9321
Duane Fourie <duanef@telesure.co.za>
2018-05-31 15:33:45 +0200

    Added Some JS

--- Javamster.js
+++ Javamster.js
@@ -1,5 +1,60 @@
-alert("Hello, World");
-var myVariable_1 = "10";
+//Simple If Statements
+/*var youLikeMeat = true;
+var myNum = 8;
 
 
+if (myNum == 10){
 
+    document.write("myNum is equal to 10");
+
+} else{
+
+    document.write("myNum is not equal to 10")
+
+}*/
+
+//Else if Statements
+
+/*var myAge = 39;
+
+if (myAge > 30) {
+
+    document.write("You are over 30!");
+} else if (myAge > 20) {
+
+    document.write("You are over 20!");
+} else if (myAge > 10) {
+
+    document.write("You are over 10!");
+} else {
+    document.write("You are not over 10!");
+}*/
+//Logical Operators and(&&) or(||)
+
+/*var myAge = 37;
+
+if (myAge >= 18 && myAge <= 30){
+    document.write("Age within Parameters");
+} else{
+    document.write("Age not within the Paramesters");
+}*/
+
+/*var myAge = 26;
+
+if (myAge < 18 || myAge > 30 || myAge === 25){
+    document.write("No");
+
+} else{
+    document.write("Yes");
+}*/
+
+//While Loops
+
+/*var age = 5;
+
+while (age < 10 ){
+    console.log("Your Age is Less tnan 10");
+    age++;
+}
+
+document.write("You are now over 10");*/
